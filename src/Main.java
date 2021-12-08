3300b548e5927a0ea7a7bc20ee4c14649ac947f1
Lazare Kvirtia <lazarekvirtia@gmail.com>
2021-12-08 19:21:08 +0400

    refactoring

--- src/Main.java
+++ src/Main.java
@@ -55,8 +55,10 @@ public class Main
         }
         */
 
-        ortAlgorithm(electricEnergeticSystem());
-        ortAlgorithm(example());
+//        ortAlgorithm(electricEnergeticSystem());
+//        ortAlgorithm(example());
+//        ortAlgorithm(example2());
+        ortAlgorithm(example3());
     }
 
 
@@ -116,6 +118,89 @@ public class Main
         HashMap<Integer, Boolean> row4 = new HashMap<>();
         row4.put(2, true);
         row4.put(5, true);
+        listX2.add(row4);
+
+        return listX2;
+
+    }
+
+    private static ArrayList<HashMap<Integer, Boolean>> example2() {
+
+        ArrayList<HashMap<Integer, Boolean>> listX2 = new ArrayList<>();
+
+        HashMap<Integer, Boolean> row1 = new HashMap<>();
+        row1.put(1, false);
+        row1.put(2, false);
+        listX2.add(row1);
+
+        HashMap<Integer, Boolean> row2 = new HashMap<>();
+        row2.put(1, false);
+        row2.put(4, false);
+        listX2.add(row2);
+
+        HashMap<Integer, Boolean> row3 = new HashMap<>();
+        row3.put(2, false);
+        row3.put(3, false);
+        listX2.add(row3);
+
+        HashMap<Integer, Boolean> row4 = new HashMap<>();
+        row4.put(3, false);
+        row4.put(4, false);
+        listX2.add(row4);
+
+        HashMap<Integer, Boolean> row5 = new HashMap<>();
+        row5.put(3, false);
+        row5.put(6, false);
+        listX2.add(row5);
+
+        HashMap<Integer, Boolean> row6 = new HashMap<>();
+        row6.put(4, false);
+        row6.put(5, false);
+        listX2.add(row6);
+
+        HashMap<Integer, Boolean> row7 = new HashMap<>();
+        row7.put(5, false);
+        row7.put(6, false);
+        listX2.add(row7);
+
+        HashMap<Integer, Boolean> row8 = new HashMap<>();
+        row8.put(1, false);
+        row8.put(6, false);
+        row8.put(8, false);
+        listX2.add(row8);
+
+        HashMap<Integer, Boolean> row9 = new HashMap<>();
+        row9.put(2, false);
+        row9.put(5, false);
+        row9.put(8, false);
+        listX2.add(row9);
+
+        return listX2;
+
+    }
+
+    private static ArrayList<HashMap<Integer, Boolean>> example3() {
+
+        ArrayList<HashMap<Integer, Boolean>> listX2 = new ArrayList<>();
+        HashMap<Integer, Boolean> row1 = new HashMap<>();
+        row1.put(5, true);
+        row1.put(8, true);
+        listX2.add(row1);
+
+        HashMap<Integer, Boolean> row2 = new HashMap<>();
+        row2.put(6, true);
+        row2.put(7, true);
+        row2.put(8, true);
+        listX2.add(row2);
+
+        HashMap<Integer, Boolean> row3 = new HashMap<>();
+        row3.put(6, true);
+        row3.put(7, true);
+        listX2.add(row3);
+
+        HashMap<Integer, Boolean> row4 = new HashMap<>();
+        row4.put(5, true);
+        row4.put(8, true);
         listX2.add(row4);
 
         return listX2;
